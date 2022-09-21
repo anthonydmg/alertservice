d5d86cc6b0a2c45bc4ab2d176b2bdc503f52abd0
Anthony Miranda <anthonymirandagil@gmail.com>
2022-09-21 15:13:44 -0500

    []

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO, send, emit
-
+from db.init import insertDataAlerDb
 from routes.alerts import alerts
 app = Flask(__name__)
 
@@ -22,7 +22,17 @@ def stopFireDetection(msg):
 @socketio.on('fireDetected')
 def fireDetected(data):
     print('Data: ', data)
-    maxTemperature = msg['maxTemperature']
-    date = msg['time']
-    
+    maxTemperature = data['maxTemperature']
+    areaFire = str(data['areaFire']) + "m"
+    latitud = str(data['latitud'])
+    longitud = str(data['longitud'])
+    altura =  str(data['distance'])
+    print(maxTemperature)
+    date = data['time']
+    insertDataAlerDb(maxTemperature, latitud, longitud, altura, areaFire)
+    print(date)
     emit('fireDetected',data, broadcast  = True)
+
+@socketio.on('radarDistance')
+def radarDistance(data):
+    print("Data: ", data)
--- db/init.py
+++ db/init.py
@@ -1,7 +1,26 @@
 import psycopg2
 
-conn = psycopg2.connect(database="fireDection",
-                        host="db_host",
-                        user="db_user",
-                        password="db_pass",
-                        port="db_port")
+
+def insertDataAlerDb(temperatura_maxima, latitud, longitud, distancia, area_region_incendiada):
+    conn = psycopg2.connect(database="fireDetection",
+                    host="127.0.0.1",
+                    user="postgres",
+                    password="admin",
+                    port="5432")
+    try:
+        cursor = conn.cursor()
+        postgres_insert_query = """ INSERT INTO fire_detection (temperatura_maxima, latitud, longitud, distancia , area_region_incendiada) VALUES (%s,%s,%s,%s,%s)"""
+        record_to_insert = (temperatura_maxima, latitud, longitud, distancia, area_region_incendiada)
+        cursor.execute(postgres_insert_query, record_to_insert)
+        conn.commit()
+        count = cursor.rowcount
+        print(count, "Record inserted successfully into fireDetection table")
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to insert record into fireDetection table", error)
+
+    finally:
+        # closing database conn.
+        if conn:
+            cursor.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
